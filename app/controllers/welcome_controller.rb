131b1c05d1cbdadacd80248bbd7824215c356619
James Pan <James@Jamess-MacBook-Pro-6.local>
2013-12-26 21:49:31 -0500

    more testing

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,14 @@
 class WelcomeController < ApplicationController
   def index
   end
+  def product
+  end
+  def product_provider
+  end
+  def product_patient
+  end
+  def about
+  end
+  def contact
+  end
 end
