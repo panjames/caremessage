20cbe95d307965fafca38f405b9a7a7b2efb2cd0
James Pan <James@Jamess-MacBook-Pro-6.local>
2013-12-27 18:56:25 -0500

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Caremessage::Application.routes.draw do
   get "welcome/index"
+  get "welcome/product"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
