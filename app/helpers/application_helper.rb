aa7fb0a222276bd5775d7c1b785c3f290b530703
James Pan <James@Jamess-MacBook-Pro-6.local>
2014-01-03 00:49:12 -0500

    started programs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def body_id
+    [controller_name, action_name].join('-')
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Caremessage::Application.routes.draw do
   get "solutions/index"
+  get "product/programs"
   get "welcome/index"
   get "welcome/product"
-    get "welcome/solutions"
+  get "welcome/solutions"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
