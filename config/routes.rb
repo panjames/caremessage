5c93f7c181acf8dcd6e6ef6a351b5a24e13560c1
James Pan <James@dn800c8808.stanford.edu>
2014-01-06 23:04:55 -0800

    started fixes

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Caremessage::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root 'welcome#index'
-
+   
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
